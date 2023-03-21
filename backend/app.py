3ed5c02ffb33a268c0318a04d1e2aa34abc35979
Martin Wolfer <wolfer_martin@yahoo.de>
2023-03-22 00:35:16 +0100

    debug

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_migrate import Migrate
-from routes import *
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:8080"}})
@@ -15,6 +14,3 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 migrate = Migrate(app, db)
-
-if __name__ == '__main__':
-    app.run(debug=True)
