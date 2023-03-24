d218baeb196f24ea68abc996478219c5bd74b0d4
Martin Wolfer <wolfer_martin@yahoo.de>
2023-03-24 12:14:21 +0100

    create db changes

--- backend/app/create_db.py
+++ backend/app/create_db.py
@@ -1,5 +1,15 @@
-from app import app
+import os
+from app import create_app
 from database import db
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+BASE_DIR = os.path.join(BASE_DIR, '..')  # Gehe ein Verzeichnis höher
+INSTANCE_DIR = os.path.join(BASE_DIR, 'instance')
+
+if not os.path.exists(INSTANCE_DIR):
+    os.makedirs(INSTANCE_DIR)
+
+app = create_app(BASE_DIR)
 
 if __name__ == '__main__':
     with app.app_context():
