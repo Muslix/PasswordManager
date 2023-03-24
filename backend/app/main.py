7ba9822ebe2a872d955a85b5e39a1a9f4d8602bf
Martin Wolfer <wolfer_martin@yahoo.de>
2023-03-24 12:12:06 +0100

    changes for db creat and access

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,14 @@
-from app import app
+import os
+from app import create_app
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+BASE_DIR = os.path.join(BASE_DIR, '..')  # Gehe ein Verzeichnis höher
+INSTANCE_DIR = os.path.join(BASE_DIR, 'instance')
+
+if not os.path.exists(INSTANCE_DIR):
+    os.makedirs(INSTANCE_DIR)
+
+app = create_app(BASE_DIR)
 
 if __name__ == '__main__':
     app.run(debug=True)
